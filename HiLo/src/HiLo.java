f1ab6df05eac63c3c1600486aa44bd06312ae136
zizoumunoz <zmunoz@outlook.es>
2020-09-29 22:40:09 -0700

    Commented out number to be guessed

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -6,7 +6,7 @@ public class HiLo {
 		Scanner myScan = new Scanner(System.in);
 		
 		int theNumber = (int)(Math.random() * 100 + 1);
-		System.out.println( theNumber );
+//		System.out.println( theNumber );
 		int guess = 0;
 		System.out.println("Guess a number between 1 and 100");
 		guess = myScan.nextInt();
