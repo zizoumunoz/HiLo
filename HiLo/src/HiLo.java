4fc229983ccfe02bfc103dae194bde7061a929dc
zizoumunoz <zmunoz@outlook.es>
2020-09-29 22:47:14 -0700

    Added while loop with if statements to check for win.

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -4,13 +4,23 @@ public class HiLo {
 
 	public static void main(String[] args) {
 		Scanner myScan = new Scanner(System.in);
-		
-		int theNumber = (int)(Math.random() * 100 + 1);
-//		System.out.println( theNumber );
+
+		int theNumber = (int) (Math.random() * 100 + 1);
+//		System.out.println(theNumber);
 		int guess = 0;
-		System.out.println("Guess a number between 1 and 100");
-		guess = myScan.nextInt();
-		System.out.println("You entered " + guess + ".");
+
+		while (guess != theNumber) {
+			System.out.println("Guess a number between 1 and 100");
+			guess = myScan.nextInt();
+			System.out.println("You entered " + guess + ".");
+			if (guess < theNumber)
+				System.out.println(guess + " is too low. Try again.");
+			else if (guess > theNumber)
+				System.out.println(guess + " is too high. Try again.");
+			else
+				System.out.println(guess + " is correct. You win!");
+		}
+
 	}
 
 }
