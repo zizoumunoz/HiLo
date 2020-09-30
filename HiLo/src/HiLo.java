47c762bc85e24de54693540aa973289d6642bb61
zizoumunoz <zmunoz@outlook.es>
2020-09-29 22:13:57 -0700

    Initial Commit

---
+++ HiLo/src/HiLo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner myScan = new Scanner(System.in);
+		
+		int theNumber = (int)(Math.random() * 100 + 1);
+		System.out.println( theNumber );
+		int guess = 0;
+		System.out.println("Guess a number between 1 and 100");
+		guess = myScan.nextInt();
+		System.out.println("You entered " + guess + ".");
+	}
+
+}
